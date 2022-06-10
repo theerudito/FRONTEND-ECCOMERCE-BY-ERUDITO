cd232e2ac0e7a0e795a56db2ea49db1cabab56a4
theerudito <erudito.tv@gmail.com>
2022-06-10 13:20:57 -0500

    v5

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -13,25 +13,25 @@ export const Menu = () => {
         </li>
 
         <li>
-          <Link className="Link" to={RoutesApps.computers}>
+          <Link className="Link" to={RoutesApps.hardware}>
             HARDWARE
           </Link>
         </li>
 
         <li>
-          <Link className="Link" to={RoutesApps.computers}>
+          <Link className="Link" to={RoutesApps.games}>
             GAMES
           </Link>
         </li>
 
         <li>
-          <Link className="Link" to={RoutesApps.computers}>
+          <Link className="Link" to={RoutesApps.software}>
             SOFTWARE
           </Link>
         </li>
 
         <li>
-          <Link className="Link" to={RoutesApps.computers}>
+          <Link className="Link" to={RoutesApps.accessories}>
             ACCESSORIES
           </Link>
         </li>
--- src/Components/Pages/Categories/Computers.jsx
+++ src/Components/Pages/Categories/Computers.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Footer } from "../../Footer/Footer";
 import { Header } from "../../Header/Header";
 import { Header2 } from "../../Header/Header2";
+import { Card } from "../../Helpers/Data";
 import { Menu } from "../../Menu/Menu";
 import { SocialMedia } from "../../SocialMedia/SocialMedia";
 
@@ -11,7 +12,33 @@ export const Computers = () => {
       <Header />
       <Header2/>
       <Menu/>
-      <div>Computers</div>
+      <h2>COMPUTERS </h2>
+      <div className="contenedorCard">
+      {Card.map((item) => (
+        <div className="bodyCard" key={item.id}>
+          <div className="containerImagen">
+            <img className="imagenCard" src={item.pic} alt="foto" />
+          </div>
+
+          <div className="containerInforCard">
+            <h1 className="name">{item.name} </h1>
+            <p className="description">{item.description} </p>
+            <p className="starts">{item.star} </p>
+
+            <div className="containerInforPrice">
+              <p className="price">{item.price} </p>
+              <p className="offert">{item.priceDesc}</p>
+              <p className="desc">{item.desc} </p>
+            </div>
+
+            <div className="buttonsAdd">
+              <button className="buttoAddCart">ADD TO CART</button>
+              <button className="moreInfo">MORE</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
       <SocialMedia/>
       <Footer />
     </>
