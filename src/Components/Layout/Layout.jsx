ef066902405800898e8a83ba012139d9feec7309
theerudito <erudito.tv@gmail.com>
2022-06-10 10:54:55 -0500

    v5

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import { Cards } from '../Cards/Cards'
+import React from "react";
+import { Cards } from "../Cards/Cards";
 
-import { Carrousel } from '../Carrousel/Carrousel'
-import { Collection } from '../Collection/Collections'
+import { Carrousel } from "../Carrousel/Carrousel";
+import { Collection } from "../Collection/Collections";
 
-import { Footer } from '../Footer/Footer'
-import { Header } from '../Header/Header'
-import { Header2 } from '../Header/Header2'
-import { Menu } from '../Menu/Menu'
-import { SocialMedia } from '../SocialMedia/SocialMedia'
-
-
+import { Footer } from "../Footer/Footer";
+import { Header } from "../Header/Header";
+import { Header2 } from "../Header/Header2";
+import { Menu } from "../Menu/Menu";
+import { SocialMedia } from "../SocialMedia/SocialMedia";
 
 export const Layout = () => {
   return (
     <>
-    <Header/>
-    
-    <Header2/>
-    <div style={{background:"white"}}>
-    <Menu/>
-    <Carrousel/>
-    <Collection/>
-    <Cards/>
-    <SocialMedia/>
-    </div>
-    <Footer/>
+      <header>
+        <Header />
+        <Header2 />
+      </header>
+      <section style={{ background: "white" }}>
+        <nav>
+          <Menu />
+        </nav>
+        <Carrousel />
+        <Collection />
+        <Cards />
+        <SocialMedia />
+      </section>
+      <footer>
+        <Footer />
+      </footer>
     </>
-  )
-}
+  );
+};
