63ced08a2ecff99d7ee448fdd726e84e28e297cb
theerudito <erudito.tv@gmail.com>
2022-06-11 13:15:25 -0500

    v6

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -9,39 +9,39 @@ export const Cards = () => {
     ...item.HEADSET,
     ...item.KEYBOARDS,
     ...item.MONITOR,
-  ]).reduce((a, b) => a.concat(b), []).slice(0, 12);
+  ])
+    .reduce((a, b) => a.concat(b), [])
+    .slice(0, 12);
 
   return (
     <div className="contenedorCard">
-      {products
-        .map((item) => (
-          <div className="bodyCard" key={item.id}>
-            <div className="containerImagen">
-              <img className="imagenCard" src={item.pic} alt="foto" />
+      {products.map((item) => (
+        <div className="bodyCard" key={item.id}>
+          <div className="containerImagen">
+            <img className="imagenCard" src={item.pic} alt="foto" />
+          </div>
+
+          <div className="containerInforCard">
+            <div className="info">
+              <h1 className="name">{item.name} </h1>
+              <p className="description">{item.description} </p>
+              <p className="marc">{item.marc} </p>
+              <p className="starts">{item.star} </p>
             </div>
 
-            <div className="containerInforCard">
-              <div className="info">
-                <h1 className="name">{item.name} </h1>
-                <p className="description">{item.description} </p>
-                <p className="marc">{item.marc} </p>
-                <p className="starts">{item.star} </p>
-              </div>
+            <div className="containerInforPrice">
+              <p className="price">{item.price} </p>
+              <p className="offert">{item.priceDesc}</p>
+              <p className="desc">{item.desc} </p>
+            </div>
 
-              <div className="containerInforPrice">
-                <p className="price">{item.price} </p>
-                <p className="offert">{item.priceDesc}</p>
-                <p className="desc">{item.desc} </p>
-              </div>
-
-              <div className="buttonsAdd">
-                <button className="buttoAddCart">ADD TO CART</button>
-                <button className="moreInfo">MORE</button>
-              </div>
+            <div className="buttonsAdd">
+              <button className="buttoAddCart">ADD TO CART</button>
+              <button className="moreInfo">MORE</button>
             </div>
           </div>
-        ))
-        }
+        </div>
+      ))}
     </div>
   );
 };
