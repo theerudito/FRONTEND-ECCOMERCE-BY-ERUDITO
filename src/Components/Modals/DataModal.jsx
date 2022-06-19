d423841b6dbcc397acd9f8cfacebf2f8d520585d
theerudito <erudito.tv@gmail.com>
2022-06-19 15:28:56 -0500

    v8

--- src/Components/Modals/DataModal.jsx
+++ src/Components/Modals/DataModal.jsx
@@ -39,14 +39,18 @@ export const DataModalBody = ({ item }) => {
             <p>Marca: HP </p>
           </div>
           <div className="containerPrices">
-            Precio
+            <p className="pri">Precio</p>
             <p>Price: 200 </p>
             <p>Desc: 8% </p>
             <p>Offer: 180 </p>
             <p>Quantity</p>
-            <input type="number" placeholder="1" />
-            <button>-</button>
-            <button>+</button>
+
+            <div className="botons">
+              <input type="number" placeholder="1" />
+              <button>-</button>
+              <button>+</button>
+            </div>
+
             <div className="contenedorBoton">
               <button>Add Cart</button>
             </div>
@@ -65,29 +69,13 @@ export const DataModalComputer = ({ item }) => {
         <div className="bodyModal">
           <div className="containerImagenModal">
             <div className="img">
-              <img
-                src={item.pic}
-                alt="foto"
-                className="imagenData"
-              />
+              <img src={item.pic} alt="foto" className="imagenData" />
             </div>
             <div className="carrosusel">
-              <img
-                src={item.pic}
-                alt="pic"
-              />
-              <img
-                src={item.pic}
-                alt="pic"
-              />
-              <img
-                src={item.pic}
-                alt="pic"
-              />
-              <img
-                src={item.pic}
-                alt="pic"
-              />
+              <img src={item.pic} alt="pic" />
+              <img src={item.pic} alt="pic" />
+              <img src={item.pic} alt="pic" />
+              <img src={item.pic} alt="pic" />
             </div>
           </div>
           <div className="containerInfor">
