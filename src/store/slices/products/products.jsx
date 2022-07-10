09bddd1ce268dacbeb8f0e583601ae535c557ab8
theerudito <erudito.tv@gmail.com>
2022-07-10 14:33:11 -0500

    v17

--- src/store/slices/products/products.jsx
+++ src/store/slices/products/products.jsx
@@ -4,10 +4,17 @@ export const productsSlice = createSlice({
   name: "products",
   initialState: {
     products: [],
+    computers: [],
+    headsets: [],
+    laptops: [],
+    monitors: [],
+    keyboards: [],
+    accessories: [],
     isLoading: true,
     oneProduct: {},
   },
   reducers: {
+    //=======================PRODUCTS==============================
     getProduct: (state, action) => {
       state.products = action.payload;
       state.isLoading = false;
@@ -15,8 +22,18 @@ export const productsSlice = createSlice({
     oneProductModal: (state,action) => {
       state.oneProduct = action.payload
     },
+    //=======================PRODUCTS==============================
+    getHeadsets: (state, action) => {
+        state.headsets = action.payload;
+        state.isLoading = false;
+    },
+    //=======================HEADSETS==============================
+    oneHeadsetModal: (state,action) => {
+      state.oneProduct = action.payload
+    },
+    //=======================HEADSETS==============================
   },
 });
 
 // actions para el reducer
-export const { getProduct, oneProductModal } = productsSlice.actions;
+export const { getProduct, oneProductModal, getHeadsets, oneHeadsetModal } = productsSlice.actions;
