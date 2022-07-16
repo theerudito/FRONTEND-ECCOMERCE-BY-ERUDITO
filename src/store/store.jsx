e3d760c42999ddbd80333215ab0d7090bf936525
theerudito <erudito.tv@gmail.com>
2022-07-16 11:13:56 -0500

    v19

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {productsSlice} from "./slices/products/";
-import {cartSlice} from "./slices/cart/";
+import { configureStore } from "@reduxjs/toolkit";
+import { productsSlice } from "./slices/products/";
+import { cartSlice } from "./slices/cart/";
 
 export const store = configureStore({
+  // the root reducer is the combination of all reducers
   reducer: {
     products: productsSlice.reducer,
     cart: cartSlice.reducer,
